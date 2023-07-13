6c8abdc8c44dc98aee809d5aee611f7f1b4a6a23
“kai0503” <1792293621@qq.com>
2023-07-13 17:55:48 +0800

    修改了页面

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 //app.js
 App({
-  // onLaunch: function () {
-  //   // 展示本地存储能力
-  //   var logs = wx.getStorageSync('logs') || []
-  //   logs.unshift(Date.now())
-  //   wx.setStorageSync('logs', logs)
-  //   wx.getSystemInfo({
-  //     success: e => {
-  //       this.globalData.StatusBar = e.statusBarHeight;
-  //       let capsule = wx.getMenuButtonBoundingClientRect();
-  //       if (capsule) {
-  //         this.globalData.Custom = capsule;
-  //         this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
-  //       } else {
-  //         this.globalData.CustomBar = e.statusBarHeight + 50;
-  //       }
-  //     }
-  //   })
-  // },
   globalData: {
   url: "https://zkqx.sdlczq.cn/xjmj/",  
    //  url: "http://192.168.3.250:8013/",
